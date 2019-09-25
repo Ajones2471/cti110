5715ddc84ff804f99f4694f7698ddd7da65ad80d
Ajones2471 <55244485+Ajones2471@users.noreply.github.com>
2019-09-24 22:38:54 -0400

    Add files via upload
    First Submission for CTI 110.

---
+++ P2T1_SalesPrediction_AndrewJones.py
@@ -0,0 +1,14 @@
+#Sales prediction tutorial
+# 09-24-2019
+# CTI-110 P2T1 - Sales Prediction
+# Andrew Jones
+
+# Get projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate profit as 23% of total sales.
+profit = total_sales * 0.23
+
+#Display the profit
+print('The profit is $',format(profit, ',.2f'))
+
